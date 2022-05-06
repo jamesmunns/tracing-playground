15474fb8f28d7246100137a98db87094cb7568ba
James Munns <james@onevariable.com>
2022-05-06 20:11:41 +0200

    Round trips!

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,26 @@ use std::{
     sync::atomic::{AtomicBool, AtomicU32, Ordering},
 };
 
-use bbqueue_sync::{Producer, GrantW, BBBuffer, Consumer};
-use rzcobs::Write;
-use serde::{Serialize, Deserialize};
+use bbqueue_sync::{BBBuffer, Consumer, GrantW, Producer};
 use core::mem::MaybeUninit;
 use groundhog::{std_timer::Timer, RollingTimer};
+use rzcobs::{Write, self};
+use serde::{Deserialize, Serialize};
 use tracing::{event, Id, Level};
 use tracing_core::{span::Current, Collect, Dispatch};
 use tracing_serde::AsSerde;
+
+// fn main() {
+//     let msg: &[u8] = &[
+//         0, 10, 111, 117, 116, 101, 114, 95, 115, 112, 97, 110, 18, 116, 114, 97, 99, 105, 110, 103,
+//         95, 112, 108, 97, 121, 103, 114, 111, 117, 110, 100, 0, 1, 18, 116, 114, 97, 99, 105, 110,
+//         103, 95, 112, 108, 97, 121, 103, 114, 111, 117, 110, 100, 1, 11, 115, 114, 99, 47, 109, 97,
+//         105, 110, 46, 114, 115, 1, 23, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//     ];
+//     let enc = rzcobs::encode(msg);
+//     let dec = rzcobs::decode(&enc).unwrap();
+//     assert_eq!(msg, &dec);
+// }
 
 fn main() {
     let cons = BQ.init().unwrap();
@@ -24,7 +36,11 @@ fn main() {
     let _ = span.enter();
     do_thing::doit();
 
+    println!("===========================");
+
     let mut data = cons.read().unwrap().to_vec();
+
+    let mut ct = 0;
 
     while !data.is_empty() {
         let pos = match data.iter().position(|b| *b == 0) {
@@ -34,14 +50,21 @@ fn main() {
                 break;
             }
         };
+
         let later = data.split_off(pos + 1);
         assert_eq!(Some(0), data.pop());
-        println!("{:?}", data);
-        let decoded = rzcobs::decode(&data[..data.len() - 1]).unwrap();
-        println!("{:?}", decoded);
+        let decoded = rzcobs::decode(&data[..data.len()]).unwrap();
+        match postcard::from_bytes::<Packet<'_>>(&decoded) {
+            Ok(p) => println!("{:?}", p),
+            Err(e) => {
+                println!(">>>{}<<<", ct);
+                println!("!!!{:?}!!!", &data);
+                println!("{:?}", &decoded);
+                panic!("{:?}", e);
+            }
+        };
         data = later;
-        let deser: Packet<'_> = postcard::from_bytes(&decoded).unwrap();
-        println!("{:?}", deser);
+        ct += 1;
     }
 }
 
@@ -54,7 +77,6 @@ pub mod do_thing {
         });
     }
 }
-
 
 static BQ: BBQCollector<Timer<1_000_000>, 16384, 512> = BBQCollector::new();
 
@@ -102,10 +124,12 @@ pub struct BBQCollector<TIMER, const TTL_SIZE: usize, const MAX_SINGLE: usize> {
     _pdt: PhantomData<TIMER>,
 }
 
-unsafe impl<TIMER, const TTL_SIZE: usize, const MAX_SINGLE: usize>
-    Sync for BBQCollector<TIMER, TTL_SIZE, MAX_SINGLE>
+unsafe impl<TIMER, const TTL_SIZE: usize, const MAX_SINGLE: usize> Sync
+    for BBQCollector<TIMER, TTL_SIZE, MAX_SINGLE>
 where
-    TIMER: RollingTimer<Tick = u32> + Default + 'static {}
+    TIMER: RollingTimer<Tick = u32> + Default + 'static,
+{
+}
 
 impl<TIMER, const TTL_SIZE: usize, const MAX_SINGLE: usize>
     BBQCollector<TIMER, TTL_SIZE, MAX_SINGLE>
@@ -151,9 +175,7 @@ where
         if !self.initialized.load(Ordering::SeqCst) {
             return Err(());
         }
-        unsafe {
-            Ok(&*self.prod.get().cast())
-        }
+        unsafe { Ok(&*self.prod.get().cast()) }
     }
 
     pub fn get_next_id(&self) -> Id {
@@ -172,12 +194,11 @@ where
     fn get_scratch<'a>(&self, _grant: &'_ GrantW<'_, TTL_SIZE>) -> &mut [u8; MAX_SINGLE] {
         // SAFETY: The presence of a write grant shows that we own an exclusive lock to the writer
         // side of the BBQ Producer, preventing re-entrancy problems.
-        unsafe {
-            (*self.scratch.get()).assume_init_mut()
-        }
+        unsafe { (*self.scratch.get()).assume_init_mut() }
     }
 
     fn encode(&self, msg: TracingWire<'_>) {
+        println!("{:?}", msg);
         // Attempt to get the buffer. Fails if it was not initialized
         let prod = if let Ok(prod) = self.get_prod() {
             prod
@@ -209,28 +230,52 @@ where
             }
         };
 
+        println!("{:?}", used);
+
+        #[derive(Debug)]
         struct RWriter<'a> {
             offset: usize,
             buf: &'a mut [u8],
         }
 
-        impl<'a> Write for RWriter<'a> {
-            type Error = ();
-
-            fn write(&mut self, byte: u8) -> Result<(), Self::Error> {
+        impl<'a> RWriter<'a> {
+            #[inline(always)]
+            fn push_one(&mut self, byte: u8) -> Result<(), ()> {
                 *self.buf.get_mut(self.offset).ok_or(())? = byte;
                 self.offset += 1;
                 Ok(())
             }
         }
 
+        impl<'a> Write for RWriter<'a> {
+            type Error = ();
+
+            #[inline(always)]
+            fn write(&mut self, byte: u8) -> Result<(), Self::Error> {
+                self.push_one(byte)
+            }
+        }
+
+        let venc = rzcobs::encode(used);
+        let vdec = rzcobs::decode(&venc).unwrap();
+        // assert_eq!(used, &vdec);
+
         // Encode to COBS for framing
-        let mut enc = rzcobs::Encoder::new(RWriter { offset: 0, buf: &mut wgr });
+        let mut enc = rzcobs::Encoder::new(RWriter {
+            offset: 0,
+            buf: &mut wgr,
+        });
         let res = used.iter().try_for_each(|b| enc.write(*b));
         if res.is_ok() {
-            if enc.writer().write(0).is_ok() {
-                let offset = enc.writer().offset;
-                wgr.commit(offset);
+            if enc.end().is_ok() {
+                if enc.writer().push_one(0).is_ok() {
+                    let offset = enc.writer().offset;
+                    println!("--{:?}", &wgr[..offset]);
+                    // assert_eq!(&venc, &wgr[..offset-1]);
+                    wgr.commit(offset);
+                } else {
+                    // TODO recover
+                }
             } else {
                 // TODO recover
             }
@@ -240,8 +285,8 @@ where
     }
 }
 
-impl<TIMER, const TTL_SIZE: usize, const MAX_SINGLE: usize>
-    Collect for BBQCollector<TIMER, TTL_SIZE, MAX_SINGLE>
+impl<TIMER, const TTL_SIZE: usize, const MAX_SINGLE: usize> Collect
+    for BBQCollector<TIMER, TTL_SIZE, MAX_SINGLE>
 where
     TIMER: RollingTimer<Tick = u32> + Default + 'static,
 {
@@ -286,4 +331,3 @@ where
         Current::unknown()
     }
 }
-
